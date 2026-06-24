719986015c3e30b81a68d271714b32c0e84232e8
Кирилл <kiryasik34@gmail.com>
2026-06-24 22:58:32 +0500

    валет

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -9,6 +9,19 @@ def test_get_balance():
     body = response.json()
 
     assert response.status_code == 200, f'должен быть 200 {response.content}'
+    assert len(body) == 0, f"негативный тест. ,balance не должен быть пустым. {response.content}"
+
+def test_get_wallets():
+    login = "kik"
+    url = "http://localhost:8000/api/v1/wallets"
+
+    response = requests.get(url, headers={"Authorization": f"Bearer {login}"})
+    body = response.json()
+
+    wallet = body[0]
+    assert response.status_code == 200
+    assert len(body) > 0
+    assert float(wallet["balance"]) == 1000.0
 
 
 
